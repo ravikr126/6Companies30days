af447cfe63460e67e489d3b4752ab89ef7519609
Ravi Kumar <ravi14577@gmail.com>
2022-01-04 18:12:58 +0530

    question 5th

---
+++ Goldman Sach/5.ugly number.cpp
@@ -0,0 +1,41 @@
+/*
+Problem Link: https://practice.geeksforgeeks.org/problems/ugly-numbers2254/1/
+Title: Ugly Numbers
+Difficulty: Hard
+Language: C++
+*/
+
+// Really a great question
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+
+class Solution{
+public:	
+	ull getNthUglyNo(int n) {
+	    set<ull> s;
+	    s.insert(1);
+	    while(--n) {
+	        auto i = s.begin();
+	        s.insert(*i * 2);
+	        s.insert(*i * 3);
+	        s.insert(*i * 5);
+	        s.erase(i);
+	    }
+	    return *s.begin();
+	}
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        auto ans = ob.getNthUglyNo(n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
